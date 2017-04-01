2125f1b8d7b5b6dec09c24cf970ade5a2f6e7506
Federico Menozzi <federicogmenozzi@gmail.com>
2017-04-01 17:46:43 -0400

    Compactify Noise.hs

--- Noise.hs
+++ Noise.hs
@@ -1,7 +1,7 @@
 import Data.Bits
 import Data.Int
 
--- Integer noise function
+-- 2D integer noise function
 inf2 :: Int32 -> Int32 -> Double
 inf2 x y = 1.0 - (fromIntegral j / 1073741824.0)
            where
@@ -20,18 +20,13 @@ lerp x y w = if w < 0 || w > 1 then
 bilerp :: Double -> Double -> Double -> Double -> Double -> Double -> Double
 bilerp ll lr ul ur wx wy = lerp lower upper wy
                          where
-                             lower = lerp ll lr wx
-                             upper = lerp ul ur wx
+                             (lower, upper) = (lerp ll lr wx, lerp ul ur wx)
 
 -- 2D value noise
 noise2 :: Double -> Double -> Double
 noise2 x y = bilerp ll lr ul ur wx wy
              where
-                 ix = floor x :: Int32
-                 iy = floor y :: Int32
-                 ll = inf2 (ix+0) (iy+0)
-                 lr = inf2 (ix+1) (iy+0)
-                 ul = inf2 (ix+0) (iy+1)
-                 ur = inf2 (ix+1) (iy+1)
-                 wx = x - fromIntegral (floor x)
-                 wy = y - fromIntegral (floor y)
+                 (ix, iy) = (floor x :: Int32, floor y :: Int32)
+                 (wx, wy) = (x - fromIntegral (floor x), y - fromIntegral (floor y))
+                 (ll, lr) = (inf2 (ix+0) (iy+0), inf2 (ix+1) (iy+0))
+                 (ul, ur) = (inf2 (ix+0) (iy+1), inf2 (ix+1) (iy+1))

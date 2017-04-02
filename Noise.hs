5ac1c843e85358a82584e1d644f769ab1526f958
Federico Menozzi <federicogmenozzi@gmail.com>
2017-04-02 10:21:34 -0400

    Add cubic/quintic interpolants, minor changes

--- Noise.hs
+++ Noise.hs
@@ -2,12 +2,12 @@ import Data.Bits
 import Data.Int
 
 -- 2D integer noise function
-inf2 :: Int32 -> Int32 -> Double
-inf2 x y = 1.0 - (fromIntegral j / 1073741824.0)
-           where
-               m = x + y*57
-               n = (shiftR m 13) `xor` m
-               j = (n * (n*n*15731 + 789221) + 1376312589) .&. 0x7fffffff
+inf :: Int32 -> Int32 -> Double
+inf x y = 1.0 - (fromIntegral j / 1073741824.0)
+        where
+            m = x + y*57
+            n = (shiftR m 13) `xor` m
+            j = (n * (n*n*15731 + 789221) + 1376312589) .&. 0x7fffffff
 
 -- Linear interpolation
 lerp :: Double -> Double -> Double -> Double
@@ -16,6 +16,18 @@ lerp x y w = if w < 0 || w > 1 then
              else
                  x*(1.0-w) + y*w
 
+-- Cubic S-curve
+cscurve :: Double -> Double -> Double -> Double
+cscurve x y w = lerp x y cw
+              where
+                  cw = ((-2)*w^3) + (3*w^2)
+
+-- Quintic S-curve
+qscurve :: Double -> Double -> Double -> Double
+qscurve x y w = lerp x y qw
+              where
+                  qw = 6*w^5 - 15*w^4 + 10*w^3
+
 -- Bilinear interpolation
 bilerp :: Double -> Double -> Double -> Double -> Double -> Double -> Double
 bilerp ll lr ul ur wx wy = lerp lower upper wy
@@ -23,10 +35,10 @@ bilerp ll lr ul ur wx wy = lerp lower upper wy
                              (lower, upper) = (lerp ll lr wx, lerp ul ur wx)
 
 -- 2D value noise
-noise2 :: Double -> Double -> Double
-noise2 x y = bilerp ll lr ul ur wx wy
+value :: Double -> Double -> Double
+value x y = bilerp ll lr ul ur wx wy
            where
                (ix, iy) = (floor x :: Int32, floor y :: Int32)
                (wx, wy) = (x - fromIntegral (floor x), y - fromIntegral (floor y))
-               (ll, lr) = (inf2 (ix+0) (iy+0), inf2 (ix+1) (iy+0))
-               (ul, ur) = (inf2 (ix+0) (iy+1), inf2 (ix+1) (iy+1))
+               (ll, lr) = (inf (ix+0) (iy+0), inf (ix+1) (iy+0))
+               (ul, ur) = (inf (ix+0) (iy+1), inf (ix+1) (iy+1))

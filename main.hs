d61ea1cd5e1ba39bebafb660794b868bb70e449d
Federico Menozzi <federicogmenozzi@gmail.com>
2017-04-05 17:21:51 -0400

    Value noise with lerp working!

--- main.hs
+++ main.hs
@@ -2,12 +2,27 @@ import Noise
 
 main = do
     -- Define grid dimensions
-    let w = 64
-        h = 64
+    let w = 256 :: Int
+        h = 256 :: Int
+
+    let wf = fromIntegral w
+        hf = fromIntegral h
+
+    let scale = 20.0
 
     -- Print PGM header info
     putStrLn "P2"
     putStrLn $ show w ++ " " ++ show h ++ " 256"
 
-    -- Output white pixel
-    mapM putStrLn (replicate (w*h) "256")
+    -- Get list of pixel coords (zero-based indexing)
+    let xy = [(x,y) | x <- [0..w-1], y <- [0..h-1]]
+
+    -- Convert pixel coords to normalized uv-coords
+    let uv = [(fromIntegral x / (wf-1) * scale, fromIntegral y / (hf-1) * scale) | (x,y) <- xy]
+
+    -- Apply noise function to each uv-coord
+    let noiseValues = [Noise.value u v | (u,v) <- uv]
+
+    -- Remap noise values to grescale values
+    let greyValues = [floor (256*(n+1)/2) | n <- noiseValues]
+    mapM (putStrLn . show) greyValues
